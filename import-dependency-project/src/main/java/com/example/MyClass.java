8566baa205d080b6dfc95a5c50abf88635106645
HP <rusobraz@mail.ru>
2025-01-04 17:11:53 +0300

    Решение ДЗ

---
+++ import-dependency-project/src/main/java/com/example/MyClass.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class MyClass {
+    public void someMethod() {
+        System.out.println("Вызов некоторого метода");
+    }
+}
--- maven-project/src/main/java/com/example/App.java
+++ maven-project/src/main/java/com/example/App.java
@@ -1,13 +1,17 @@
 package com.example;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        String str = "Hello world!";
+        System.out.println(str);
+        System.out.println(StringUtils.reverse(str));
+        MyClass myClass = new MyClass();
+        myClass.someMethod();
     }
 }
